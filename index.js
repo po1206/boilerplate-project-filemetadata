c27b3a92e3b7918c68f9f5835570044063f52c10
Good Luck <dibiasejustin40@gmail.com>
2025-03-23 14:49:08 +0000

    file upload

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var cors = require('cors');
+const multer = require("multer");
 require('dotenv').config()
 
 var app = express();
@@ -7,11 +8,24 @@ var app = express();
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
+
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
+const upload = multer({dest: 'uploads/'});
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  console.log(req.file);
+    if(!req.file) {
+      return res.status(400).json({error: 'No file uploaded'});
+    }
+    console.log(req.file.originalname, req.file.mimetype, req.file.size);
+    res.json({
+      name: req.file.originalname,
+      type: req.file.mimetype,
+      size: req.file.size
+    });
+})
 
 
 const port = process.env.PORT || 3000;
